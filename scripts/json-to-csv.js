0c07acbc7402bdec4411b972a6916d4653bdf4b1
Lucas Suzigan <50274831+lsuzigan@users.noreply.github.com>
2020-07-29 23:25:09 -0300

    Simple Json parsing complete
    (doesn't support nested objects)

--- scripts/json-to-csv.js
+++ scripts/json-to-csv.js
@@ -1,17 +1,41 @@
 function fillCsv(csv) {
     document.getElementById('csv').value = csv
-    console.log(csv)
 }
 
 function convertJson(json) {
-    const parsedJson = JSON.parse(json) //object with the parsed json
-    fillCsv(parsedJson)
+    let headLine = ''
+    let rowLine = ''
+    let rows = ''
+    let csv = ''
+
+    const parsedJson = JSON.parse(json) //array of objects
+    for (let i in parsedJson[0]) {
+        headLine += '"' + i + '",'
+    }
+
+    headLine = headLine.slice(0, -1)
+    headLine += '\r\n'
+    console.log(`'${headLine}'`)
+
+    for (let i = 0; i < parsedJson.length; i++) {
+        rowLine = ''
+        for (let j in parsedJson[i]) {
+            rowLine += '"' + parsedJson[i][j] + '",'
+        }
+        rowLine = rowLine.slice(0, -1)
+        rowLine += '\r\n'
+        rows += rowLine
+        console.log(rows)
+    }
+
+    csv = headLine + rows
+    console.log(csv)
+    fillCsv(csv)
 }
 
 const submit = document.querySelector('button[send]')
 submit.onclick = function(event) {
     event.preventDefault()
-
     const json = document.getElementById('json').value
     convertJson(json)
 }
